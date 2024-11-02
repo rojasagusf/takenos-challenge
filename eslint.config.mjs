bcf76bdcb2dafbf165b9897bd88d53848f7d7c45
Agustin rojas <rojasagusf01@gmail.com>
2024-11-02 19:34:39 -0300

    init structure

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ['**/*.{js,mjs,cjs,ts}'], rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    '@typescript-eslint/no-namespace': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
