5da1247711b24268d417438cdd0a0d0806d95852
Rojas <rojasagusf01@gmail.com>
2024-11-03 17:47:23 -0300

    Separate logic

---
+++ lib/controllers/data-analysis.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import Transaction from '../models/transaction.model';
+import { sequelize } from '../models';
+import logger from '../logger';
+
+export async function getTopMerchants(_req: Request, res: Response) {
+  try {
+    const topMerchants = await Transaction.findAll({
+      attributes:[
+        'merchant',
+        [sequelize.fn('SUM', sequelize.col('amount')), 'totalAmount'],
+      ],
+      group: ['merchant'],
+      order: [[sequelize.fn('SUM', sequelize.col('amount')), 'DESC']],
+      limit: 10,
+    });
+
+    return res.status(200).json(topMerchants);
+  } catch (error) {
+    logger.error(`getTopMerchants error: ${(error as Error).message}`);
+    return res.status(500).json({
+      code: 'internal_error',
+      message: 'Internal error'
+    });
+  }
+}
